fbb3cd74a037803ca99f21782b0d5b76c92e40f5
davidladkins <david@dadkins.net>
2017-04-18 13:24:46 -0400

    problems with processing json fixed

--- app/movieServer.js
+++ app/movieServer.js
@@ -1,13 +1,54 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var fs = require("fs");
 
-app.get('/movies', function (req, res) {
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8082; 
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+router.get('/movies', function(req, res) {
 	 fs.readFile( __dirname + "/" + "movies.json", 'utf8', function (err, data) {
 			 console.log( data );
-			 res.end( data );
+          res.end(data);
+          //res.json({ message: data });   
 	 });
-})
+   
+});
+router.route('/movie')
+ .post(function(req, res) {
+    console.log(req.body.id);    
+   // First read existing movies.
+     fs.readFile( __dirname + "/" + "movies.json", 'utf8', function (err, data) {
+         data = JSON.parse( data );
+	      data["movie"+req.body.id] = req.body;
+      console.log(data);   
+          
+     fs.writeFile(__dirname + "/" + "movies.json", JSON.stringify(data), function (err) {
+         if (err) return console.log(err);
+         
+      })
+
+
+
+
+
+
+   });
+
+   res.json({ message: 'movie ' + req.body.id + ' created!' });
+});
+
+
+
+
+
 
 app.post('/movie', function (req, res) {
    // First read existing movies.
@@ -15,11 +56,11 @@ app.post('/movie', function (req, res) {
    
        data = JSON.parse( data );
 	  
-	  console.log(req.body);      // your JSON
-      res.send(req.body);
+	  console.log(req);      // your JSON
+     // res.send(req);
 	   //data["movie"+req.body.id] = movie["movie"+req.body.id];
       
-      // res.end(req);
+      res.end(JSON.stringify(data));
    });
 })
 
@@ -43,8 +84,11 @@ app.get('/movie/:id', function (req, res) {
    });
 })
 
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
 
-var server = app.listen(8082, function () {
+var server = app.listen(port, function () {
 
 	var host = server.address().address
 	var port = server.address().port
